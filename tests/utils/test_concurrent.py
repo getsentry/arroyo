681be87569770442d7e65bd041371ea0d51f0652
Lyn Nagara <lyn.nagara@gmail.com>
2022-07-19 09:34:33 -0700

    feat: Remove the synchronized consumer (#81)
    The synchronized consumer should not be part of Arroyo. It was previously
    used only by Snuba's legacy subscriptions infrastructure. This is now
    rebuilt using regular (non-synchronized) consumers for performance reasons.
    Because of those performance reasons, this is not really a pattern we want to
    encourage by providing it out of the box in this library.
    
    `arroyo.utils.synchronized` is now `arroyo.utils.commit`. Importing from
    `arroyo.utils.synchronized` is still temporarily supported and will be removed
    in a future version of this library.

--- tests/utils/test_concurrent.py
+++ tests/utils/test_concurrent.py
@@ -4,7 +4,7 @@ from concurrent.futures import TimeoutError
 
 import pytest
 
-from arroyo.utils.concurrent import Synchronized, execute
+from arroyo.utils.concurrent import execute
 
 
 def test_execute() -> None:
@@ -15,21 +15,3 @@ def test_execute() -> None:
 
     with pytest.raises(TimeoutError):
         assert execute(lambda: time.sleep(10), daemon=True).result(timeout=0)
-
-
-def test_synchronized() -> None:
-    value = object()
-    wrapper = Synchronized(value)
-
-    with wrapper.get() as wrapped:
-        assert wrapped is value
-
-    wrapper.set(object())
-
-    with wrapper.get() as wrapped:
-        assert wrapped is not value
-
-    wrapper.set(value)
-
-    with wrapper.get() as wrapped:
-        assert wrapped is value
