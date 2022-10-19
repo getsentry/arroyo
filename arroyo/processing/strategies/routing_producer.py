77cda91146d696cc5f67262bd9661cf4f82fe8d3
Nikhar Saxena <nikhar.saxena@sentry.io>
2022-10-19 12:07:35 -0700

    Add more documentation

--- arroyo/processing/strategies/routing_producer.py
+++ arroyo/processing/strategies/routing_producer.py
@@ -3,10 +3,10 @@ import time
 from abc import ABC, abstractmethod
 from collections import deque
 from concurrent.futures import Future
-from typing import Deque, MutableMapping, MutableSequence, NamedTuple, Optional, Tuple
+from typing import Deque, MutableMapping, NamedTuple, Optional, Tuple
 
 from arroyo.backends.abstract import Producer
-from arroyo.backends.kafka import KafkaPayload, KafkaProducer
+from arroyo.backends.kafka import KafkaPayload
 from arroyo.processing.strategies import ProcessingStrategy
 from arroyo.types import Commit, Message, Partition, Position, Topic
 
@@ -25,31 +25,34 @@ class MessageRoute(NamedTuple):
 
 class MessageRouter(ABC):
     """
-    Router that determines how to route messages to the correct destination.
-    """
+    An abstract class that defines the interface for a message router. A message
+    router relies on the implementation of the get_route_for_message method
+    to perform the actual routing.
 
-    @abstractmethod
-    def populate_routes(self) -> MutableSequence[KafkaProducer]:
-        """
-        This method must return a sequence of producers on which data can be
-        sent to. This is needed so that the strategy can perform maintenance
-        on all the producers like calling poll() periodically, flushing all
-        of them on shutdown, etc.
-        """
-        raise NotImplementedError
+    Some examples of what message routers could do include:
+    1. A message router that routes messages to a single topic. This is
+    similar to not having a message router at all.
+    2. A message router that performs round robin routing to a set of topics.
+    3. A message router that routes messages to a topic based on which slice
+    the message needs to be sent to.
+    """
 
     @abstractmethod
     def get_route_for_message(self, message: Message[KafkaPayload]) -> MessageRoute:
         """
-        This method must return the MessageRoute on which the
-        message should be produced. Implementations of this method can vary
-        based on the specific use case. For example, if there is no message
-        routing needed, this method can simply return the single producer and
-        topic tuple. If there are multiple producers and topics,
-        the implementation can use the message payload to determine which
-        producer and topic to use. Use case like Slicing, can use org_id passed
-        in via message headers to determine the (producer, topic) tuple to send
-        the message to.
+        This method must return the MessageRoute on which the message should
+        be produced. Implementations of this method can vary based on the
+        specific use case.
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def shutdown(self, timeout: Optional[float] = None) -> None:
+        """
+        Shutdown the router. This method is called when join() is called on
+        the strategy. This method can be used to perform any cleanup needed
+        by the router. Most often that would always include closing the
+        producers that are used by the router.
         """
         raise NotImplementedError
 
@@ -57,50 +60,38 @@ class MessageRouter(ABC):
 class RoutingProducerStep(ProcessingStrategy[KafkaPayload]):
     """
     This strategy is used to route messages to different producers/topics
-    based on the message payload. It relies on the implementation of the
-    message router to determine the producer and topic to route the message to.
+    based on the message payload. It delegates the routing logic to the
+    message router class which is passed in as a parameter. Please look at
+    the documentation for the MessageRouter class for more details.
+
+    The strategy also does not own any producers. The producers are owned by
+    the message router. As such, the strategy does not close the producers.
+    So it is important that the message router closes the producers when its
+    shutdown method is called. shutdown() is not called during the close()
+    method as it has to be non-blocking. The shutdown() method is therefore
+    called during the join() method.
     """
 
     def __init__(
         self,
         commit_function: Commit,
-        commit_max_batch_size: int,
-        commit_max_batch_time: float,
         message_router: MessageRouter,
     ) -> None:
         self.__commit_function = commit_function
-        self.__commit_max_batch_size = commit_max_batch_size
-        self.__commit_max_batch_time = commit_max_batch_time
         self.__message_router = message_router
-        self.__all_producers = self.__message_router.populate_routes()
         self.__closed = False
         self.__offsets_to_be_committed: MutableMapping[Partition, Position] = {}
-        self.__messages_processed_since_last_commit = 0
-        self.__started = time.time()
         self.__queue: Deque[
             Tuple[Message[KafkaPayload], Future[Message[KafkaPayload]]]
         ] = deque()
 
-    def _should_commit(self) -> bool:
-        now = time.time()
-        duration = now - self.__started
-        if self.__messages_processed_since_last_commit >= self.__commit_max_batch_size:
-            logger.info(
-                f"Max size reached: total of {self.__messages_processed_since_last_commit} messages after {duration:.{2}f} seconds"
-            )
-            return True
-        if now >= (self.__started + self.__commit_max_batch_time):
-            logger.info(
-                f"Max time reached: total of {self.__messages_processed_since_last_commit} messages after {duration:.{2}f} seconds"
-            )
-            return True
-
-        return False
-
     def poll(self) -> None:
         """
-        KafkaProducer's call poll() periodically in a thread. So we don't
-        need to explicitly call poll() here.
+        Periodically check which messages have been produced successfully and
+        call the commit function for the offsets that have been processed.
+        In order to commit offsets we need strict ordering, hence we stop
+        processing the queue of pending futures as soon as we encounter one
+        which is not yet completed.
         """
         while self.__queue:
             message, future = self.__queue[0]
@@ -113,18 +104,19 @@ class RoutingProducerStep(ProcessingStrategy[KafkaPayload]):
                 raise exc
 
             self.__queue.popleft()
-            self.__messages_processed_since_last_commit += 1
             self.__offsets_to_be_committed[message.partition] = Position(
                 message.next_offset, message.timestamp
             )
 
-        if self._should_commit():
-            self.__commit_function(self.__offsets_to_be_committed)
-            self.__messages_processed_since_last_commit = 0
-            self.__offsets_to_be_committed = {}
-            self.__started = time.time()
+        self.__commit_function(self.__offsets_to_be_committed)
 
     def submit(self, message: Message[KafkaPayload]) -> None:
+        """
+        This is where the actual routing happens. Each message is passed to the
+        message router to get the producer and topic to which the message should
+        be sent. The message is then sent to the producer and the future is
+        stored in the queue.
+        """
         assert not self.__closed
         producer, topic = self.__message_router.get_route_for_message(message)
         self.__queue.append(
@@ -136,20 +128,29 @@ class RoutingProducerStep(ProcessingStrategy[KafkaPayload]):
 
     def close(self) -> None:
         self.__closed = True
-        for producer in self.__all_producers:
-            producer.close()
 
     def join(self, timeout: Optional[float] = None) -> None:
         """
-        When close() is called on the strategy, all the producers are closed.
-        The producers also have logic to flush when close is called. Hence,
-        we don't need explicit call to producer flush here.
+        In addition to flushing the queue, this method also calls the
+        shutdown of the router.
         """
-        if self.__messages_processed_since_last_commit:
-            logger.info(
-                f"Committing {self.__messages_processed_since_last_commit} messages..."
-            )
-            self.__commit_function(self.__offsets_to_be_committed)
-            self.__messages_processed_since_last_commit = 0
-            self.__offsets_to_be_committed = {}
-            self.__started = time.time()
+        start = time.time()
+
+        # Commit all previously staged offsets
+        self.__commit_function({}, force=True)
+
+        while self.__queue:
+            remaining = timeout - (time.time() - start) if timeout is not None else None
+            if remaining is not None and remaining <= 0:
+                logger.warning(f"Timed out with {len(self.__queue)} futures in queue")
+                break
+
+            message, future = self.__queue.popleft()
+            future.result(remaining)
+            offset = {message.partition: message.position_to_commit}
+
+            logger.info("Committing offset: %r", offset)
+            self.__commit_function(offset, force=True)
+
+        remaining = timeout - (time.time() - start) if timeout is not None else None
+        self.__message_router.shutdown(remaining)
