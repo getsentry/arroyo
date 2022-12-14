ed348dca84f862a97711292b94ec31a68f11f491
Markus Unterwaditzer <markus-honeypot@unterwaditzer.net>
2022-12-14 16:56:34 +0100

    stage_positions cannot be conditional my guy!

--- arroyo/processing/processor.py
+++ arroyo/processing/processor.py
@@ -155,6 +155,8 @@ class StreamProcessor(Generic[TPayload]):
         If force is passed, commit immediately and do not throttle. This should
         be used during consumer shutdown where we do not want to wait before committing.
         """
+        self.__consumer.stage_positions(positions)
+
         messages_since_last_commit = 0
         for partition, pos in positions.items():
             prev_offset = self.__committed_offsets.setdefault(partition, pos.offset - 1)
@@ -166,7 +168,6 @@ class StreamProcessor(Generic[TPayload]):
         if force or self.__commit_policy.should_commit(
             elapsed, messages_since_last_commit
         ):
-            self.__consumer.stage_positions(positions)
             self.__consumer.commit_positions()
             logger.debug(
                 "Waited %0.4f seconds for offsets to be committed to %r.",
