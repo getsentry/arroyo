d56c06741173d1d48fc4dd774a6ffb1754e33ee8
Lyn <lyn.nagara@gmail.com>
2023-02-14 16:09:28 -0800

    ref: Don't alias ProcessingStrategy as ProcessingStep
    We do this inconsistently, let's simply import this as ProcessingStrategy everywhere
    without the alias.

--- arroyo/processing/strategies/run_task.py
+++ arroyo/processing/strategies/run_task.py
@@ -24,9 +24,7 @@ from typing import (
     TypeVar,
 )
 
-from arroyo.processing.strategies.abstract import MessageRejected
-from arroyo.processing.strategies.abstract import ProcessingStrategy
-from arroyo.processing.strategies.abstract import ProcessingStrategy as ProcessingStep
+from arroyo.processing.strategies.abstract import MessageRejected, ProcessingStrategy
 from arroyo.processing.strategies.dead_letter_queue.invalid_messages import (
     InvalidMessage,
     InvalidMessages,
@@ -381,11 +379,13 @@ def parallel_run_task_worker_apply(
     )
 
 
-class RunTaskWithMultiprocessing(ProcessingStep[TPayload], Generic[TPayload, TResult]):
+class RunTaskWithMultiprocessing(
+    ProcessingStrategy[TPayload], Generic[TPayload, TResult]
+):
     def __init__(
         self,
         function: Callable[[Message[TPayload]], TResult],
-        next_step: ProcessingStep[TResult],
+        next_step: ProcessingStrategy[TResult],
         num_processes: int,
         max_batch_size: int,
         max_batch_time: float,
