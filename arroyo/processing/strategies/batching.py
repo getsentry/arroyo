af0401e1fd3c820496367806ae9bc7337cfd725d
Lyn <lyn.nagara@gmail.com>
2023-03-10 16:22:14 -0800

    fix lint error

--- arroyo/processing/strategies/batching.py
+++ arroyo/processing/strategies/batching.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import MutableSequence, Optional, Sequence, Union
+from typing import MutableSequence, Optional, Union
 
 from arroyo.processing.strategies.abstract import ProcessingStrategy
 from arroyo.processing.strategies.reduce import Reduce
