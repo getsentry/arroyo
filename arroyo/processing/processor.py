e4fa164fef956f6de5a9db5eeef350e1a64d54fe
Lyn <lyn.nagara@gmail.com>
2023-03-29 15:53:39 -0700

    no more maybe committing in stream processor

--- arroyo/processing/processor.py
+++ arroyo/processing/processor.py
@@ -4,7 +4,7 @@ import logging
 import time
 from collections import defaultdict, deque
 from enum import Enum
-from typing import Deque, Generic, Mapping, MutableMapping, Optional, Sequence, cast
+from typing import Deque, Generic, Mapping, MutableMapping, Optional, Sequence
 
 from arroyo.backends.abstract import Consumer
 from arroyo.commit import CommitPolicy
@@ -139,9 +139,6 @@ class StreamProcessor(Generic[TStrategyPayload]):
         self.__buffered_messages: BufferedMessages[TStrategyPayload] = BufferedMessages(
             dlq_policy
         )
-        # Last staged offsets
-        self.__committed_offsets: MutableMapping[Partition, int] = {}
-        self.__dlq_offsets: MutableMapping[Partition, int] = {}
 
         def _close_strategy() -> None:
             if self.__processing_strategy is None:
@@ -178,7 +175,6 @@ class StreamProcessor(Generic[TStrategyPayload]):
         def on_partitions_assigned(partitions: Mapping[Partition, int]) -> None:
             logger.info("New partitions assigned: %r", partitions)
             self.__buffered_messages.reset()
-            self.__committed_offsets = cast(MutableMapping[Partition, int], partitions)
             if partitions:
                 if self.__processing_strategy is not None:
                     logger.exception(
@@ -217,8 +213,6 @@ class StreamProcessor(Generic[TStrategyPayload]):
         be used during consumer shutdown where we do not want to wait before committing.
         """
         self.__consumer.stage_offsets(offsets)
-        # We maintain a separate copy of staged offsets for the DLQ
-        self.__committed_offsets.update(offsets)
         now = time.time()
 
         if force or self.__commit_policy_state.should_commit(
@@ -267,31 +261,6 @@ class StreamProcessor(Generic[TStrategyPayload]):
             # XXX: This blocks until the message is produced. This will be slow
             # if there is a very large volume of invalid messages to be produced.
             self.__dlq_policy.producer.produce(invalid_message).result()
-            self.__dlq_offsets[exc.partition] = exc.offset + 1
-
-    def _maybe_commit_dlq_offsets(self) -> None:
-        # Commits the offset of messages sent to DLQ only if it is exactly one
-        # offset ahead of the last committed offset
-        offsets_to_commit: MutableMapping[Partition, int] = {}
-
-        for partition in list(self.__dlq_offsets.keys()):
-            dlq_offset = self.__dlq_offsets[partition]
-            committed_offset = self.__committed_offsets[partition]
-
-            if dlq_offset == committed_offset + 1:
-                offsets_to_commit[partition] = dlq_offset
-                self.__dlq_offsets.pop(partition)
-            elif dlq_offset < committed_offset:
-                logger.warning(
-                    "DLQ offset for %r is behind the committed offset",
-                    partition,
-                )
-                self.__dlq_offsets.pop(partition)
-
-        if offsets_to_commit:
-            self.__commit(offsets_to_commit, force=True)
-
-        self.__committed_offsets.update(offsets_to_commit)
 
     def _run_once(self) -> None:
         message_carried_over = self.__message is not None
@@ -333,7 +302,6 @@ class StreamProcessor(Generic[TStrategyPayload]):
                     if not message_carried_over:
                         self.__buffered_messages.append(self.__message)
                     self.__processing_strategy.submit(message)
-                    self._maybe_commit_dlq_offsets()
 
                     self.__metrics_buffer.increment(
                         ConsumerTiming.CONSUMER_PROCESSING_TIME,
