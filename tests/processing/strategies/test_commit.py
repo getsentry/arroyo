59252b0b8c6c6001fd1e43b89b0e5f9523aec559
Markus Unterwaditzer <markus-honeypot@unterwaditzer.net>
2023-06-20 19:22:03 +0200

    fix: Commit offsets even when topic is empty (#259)

--- tests/processing/strategies/test_commit.py
+++ tests/processing/strategies/test_commit.py
@@ -11,3 +11,15 @@ def test_commit() -> None:
     strategy.submit(Message(Value(b"", {Partition(Topic("topic"), 1): 5})))
 
     assert commit_func.call_count == 1
+
+
+def test_commit_poll() -> None:
+    # This is currently necessary to ensure that offsets are committed (still
+    # debounced) even when the topic is empty.
+    commit_func = Mock()
+    strategy = CommitOffsets(commit_func)
+
+    strategy.poll()
+
+    assert commit_func.call_count == 1
+
