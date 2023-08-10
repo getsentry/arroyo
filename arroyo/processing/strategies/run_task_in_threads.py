e6c6ec1a6d999ed8652e240e714605abc37a91f7
Markus Unterwaditzer <markus-honeypot@unterwaditzer.net>
2023-08-10 17:01:35 +0200

    fix(run_task_in_threads): Commit offsets even when consumer is idle (#276)

--- arroyo/processing/strategies/run_task_in_threads.py
+++ arroyo/processing/strategies/run_task_in_threads.py
@@ -85,6 +85,15 @@ class RunTaskInThreads(
 
     def poll(self) -> None:
         self.__forward_invalid_offsets()
+
+        if not self.__queue:
+            # specifically for the case where we are idling, poll the next step
+            # so that committing can occur. if there is stuff in the queue and
+            # we are waiting for a future to be finished, we do not really need
+            # to forward polls.
+            self.__next_step.poll()
+            return
+
         while self.__queue:
             message, future = self.__queue[0]
             next_message: Message[TResult]
