1d4cc94fbd5419d65d02a25f1c551ac9ccc7edf7
Lyn <lyn.nagara@gmail.com>
2023-09-27 10:43:27 -0700

    ref: Use float in commit codec instead of complex datetime format
    I really don't want to port this to Rust, let's simplify the commit
    log format
    
    Also avoid putting important information in headers. This was a hack
    to avoid changing the message format earlier, but it should be avoided.
    The timestamp is now in the payload of the message

--- tests/backends/test_commit.py
+++ tests/backends/test_commit.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 from arroyo.backends.kafka.commit import CommitCodec
+from arroyo.backends.kafka import KafkaPayload
 from arroyo.commit import Commit
 from arroyo.types import Partition, Topic
 
@@ -21,3 +22,9 @@ def test_encode_decode() -> None:
     encoded = commit_codec.encode(commit)
 
     assert commit_codec.decode(encoded) == commit
+
+def test_decode_legacy() -> None:
+    legacy = KafkaPayload(b"topic:0:leader-a", b"5", [('orig_message_ts', b'2023-09-26T21:58:14.191325Z')])
+    decoded = CommitCodec().decode(legacy)
+    assert decoded.offset == 5
+    assert decoded.group == "leader-a"
