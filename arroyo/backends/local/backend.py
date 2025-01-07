47706e0eb4059d0b74643b4bb845105a95ed7d9e
Lyn Nagara <lynnagara@users.noreply.github.com>
2025-01-07 12:40:47 -0800

    let black do it's thing

--- arroyo/backends/local/backend.py
+++ arroyo/backends/local/backend.py
@@ -38,9 +38,9 @@ class LocalBroker(Generic[TStrategyPayload]):
         self.__message_storage = message_storage
         self.__clock = clock
 
-        self.__offsets: MutableMapping[
-            str, MutableMapping[Partition, int]
-        ] = defaultdict(dict)
+        self.__offsets: MutableMapping[str, MutableMapping[Partition, int]] = (
+            defaultdict(dict)
+        )
 
         # The active subscriptions are stored by consumer group as a mapping
         # between the consumer and it's subscribed topics.
