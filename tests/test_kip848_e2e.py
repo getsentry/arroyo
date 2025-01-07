7b5f1ba675b1f4b6f630ef2e9f3c10749ba5a616
Lyn Nagara <lynnagara@users.noreply.github.com>
2025-01-07 12:18:43 -0800

    assert strategy was called 30 times

--- tests/test_kip848_e2e.py
+++ tests/test_kip848_e2e.py
@@ -6,7 +6,6 @@ from contextlib import closing
 import os
 import signal
 import threading
-# import random
 import logging
 from typing import Iterator, Mapping
 
@@ -43,27 +42,28 @@ def get_topic(
         assert future.result() is None
 
 
-def print_msg(message: Message[Any]) -> Message[Any]:
-    ((partition, offset),) = message.committable.items()
-    print(f"message: {partition.index}-{offset}")
-    return message
-
-
-class Factory(ProcessingStrategyFactory[KafkaPayload]):
-    def create_with_partitions(self, commit: Commit, partitions: Mapping[Partition, int]) -> ProcessingStrategy[KafkaPayload]:
-        print("assign: ", [p.index for p in partitions])
-        return Strat(print_msg, CommitOffsets(commit))
-
-
-class Strat(RunTask[Any, Any]):
-    def join(self, *args: Any, **kwargs: Any) -> None:
-        print("joining strategy, sleeping 5 seconds")
-        time.sleep(5)
-        print("joining strategy, sleeping 5 seconds -- DONE")
-        return super().join(*args, **kwargs)
-
-
 def test_kip848_e2e() -> None:
+    counter = 0
+
+    def print_msg(message: Message[Any]) -> Message[Any]:
+        nonlocal counter
+        ((partition, offset),) = message.committable.items()
+        print(f"message: {partition.index}-{offset}")
+        counter += 1
+        return message
+
+
+    class Strat(RunTask[Any, Any]):
+        def join(self, *args: Any, **kwargs: Any) -> None:
+            print("joining strategy, sleeping 5 seconds")
+            time.sleep(5)
+            print("joining strategy, sleeping 5 seconds -- DONE")
+            return super().join(*args, **kwargs)
+
+    class Factory(ProcessingStrategyFactory[KafkaPayload]):
+        def create_with_partitions(self, commit: Commit, partitions: Mapping[Partition, int]) -> ProcessingStrategy[KafkaPayload]:
+            print("assign: ", [p.index for p in partitions])
+            return Strat(print_msg, CommitOffsets(commit))
 
     default_config = {
         "bootstrap.servers": os.environ.get("DEFAULT_BROKERS", "localhost:9092")
@@ -74,7 +74,7 @@ def test_kip848_e2e() -> None:
         producer = KafkaProducer(default_config)
 
         with closing(producer):
-            for i in range(100):
+            for i in range(30):
                 message = KafkaPayload(None, i.to_bytes(1, "big"), [])
                 producer.produce(topic, message).result()
 
@@ -103,14 +103,15 @@ def test_kip848_e2e() -> None:
         signal.signal(signal.SIGINT, handler)
         signal.signal(signal.SIGTERM, handler)
 
-        def shutdown_randomly() -> None:
-            # s = random.randint(30, 120)
-            s = 5  # tmp: shutdown after 5 seconds
+        def shutdown_after_2_seconds() -> None:
+            s = 2
             time.sleep(s)
             print(f"shutting down after {s} seconds")
             processor.signal_shutdown()
 
-        t = threading.Thread(target=shutdown_randomly, daemon=True)
+        t = threading.Thread(target=shutdown_after_2_seconds, daemon=True)
         t.start()
 
         processor.run()
+
+        assert counter == 30
