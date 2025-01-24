568f02f8a814a2251893a4d973a5a807ac832a6b
Volo Kluev <volo.kluev@sentry.io>
2025-01-24 15:39:56 -0800

    gotta be kidding me

--- arroyo/utils/metric_defs.py
+++ arroyo/utils/metric_defs.py
@@ -98,8 +98,7 @@ MetricName = Literal[
     "arroyo.processing.strategies.healthcheck.touch",
     # Counter: Number of messages dropped in the FilterStep strategy
     "arroyo.strategies.filter.dropped_messages",
-
-    # how many messages are dropped due to errors producing to the dlq
+    # Counter: how many messages are dropped due to errors producing to the dlq
     "arroyo.consumer.dlq.dropped_messages",
 
 ]
