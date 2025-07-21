6fb4ca45ad33336ed346af1276efd6ea747b38af
Evan Hicks <evanh@users.noreply.github.com>
2025-07-21 12:24:53 -0400

    fix(slo): Add error codes to producer failure in Rust (#472)
    This adds the error codes to the producer failure metric in Rust. Some new producer errors were also
    added that explicitly deal with producing (similar to the consumer errors).

--- rust-arroyo/src/backends/kafka/errors.rs
+++ rust-arroyo/src/backends/kafka/errors.rs
@@ -1,6 +1,7 @@
 use rdkafka::error::{KafkaError, RDKafkaErrorCode};
 
 use crate::backends::ConsumerError;
+use crate::backends::ProducerError;
 
 impl From<KafkaError> for ConsumerError {
     fn from(err: KafkaError) -> Self {
@@ -11,6 +12,25 @@ impl From<KafkaError> for ConsumerError {
                 }
             }
             other => ConsumerError::BrokerError(Box::new(other)),
+        }
+    }
+}
+
+impl From<KafkaError> for ProducerError {
+    fn from(err: KafkaError) -> Self {
+        match err {
+            KafkaError::MessageProduction(_) => {
+                let code = err.rdkafka_error_code().unwrap();
+                ProducerError::MessageProductionFailed { code }
+            }
+            KafkaError::Flush(_) => {
+                let code = err.rdkafka_error_code().unwrap();
+                ProducerError::FlushFailed { code }
+            }
+            other => {
+                let code = other.rdkafka_error_code().unwrap();
+                ProducerError::BrokerError { code }
+            }
         }
     }
 }
