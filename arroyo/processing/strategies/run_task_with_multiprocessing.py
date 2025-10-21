62e7bb525922995bbebc5330d42e7dcdbdd8c4b8
Mark Story <mark@mark-story.com>
2025-10-21 16:38:17 +0200

    fix: Raise errors when multiprocessing pool has been stuck for a while (#488)
    We've seen a scenario come up in one consumer where the main process
    continues to live, but the multiprocessing pool becomes stuck and
    doesn't recover. If we've been stuck for 3 minutes, we should raise
    an error to crash the consumer.
    
    Refs INC-1457

--- arroyo/processing/strategies/run_task_with_multiprocessing.py
+++ arroyo/processing/strategies/run_task_with_multiprocessing.py
@@ -41,6 +41,7 @@ DEFAULT_INPUT_BLOCK_SIZE = 16 * 1024 * 1024
 DEFAULT_OUTPUT_BLOCK_SIZE = 16 * 1024 * 1024
 
 LOG_THRESHOLD_TIME = 20  # In seconds
+FAIL_THRESHOLD_TIME = 180  # In seconds
 
 
 class ChildProcessTerminated(RuntimeError):
@@ -600,6 +601,7 @@ class RunTaskWithMultiprocessing(
             "arroyo.strategies.run_task_with_multiprocessing.batches_in_progress",
         )
         self.__pool_waiting_time: Optional[float] = None
+        self.__pool_waiting_log_time: Optional[float] = None
         self.__metrics.gauge(
             "arroyo.strategies.run_task_with_multiprocessing.processes", num_processes
         )
@@ -673,22 +675,31 @@ class RunTaskWithMultiprocessing(
                 if deadline is None or deadline > time.time():
                     continue
                 break
-            except multiprocessing.TimeoutError:
-                if self.__pool_waiting_time is None:
-                    self.__pool_waiting_time = time.time()
+            except multiprocessing.TimeoutError as err:
+                current_time = time.time()
+                if (
+                    self.__pool_waiting_time is None
+                    or self.__pool_waiting_log_time is None
+                ):
+                    self.__pool_waiting_time = current_time
+                    self.__pool_waiting_log_time = current_time
                 else:
-                    current_time = time.time()
-                    if current_time - self.__pool_waiting_time > LOG_THRESHOLD_TIME:
+                    if current_time - self.__pool_waiting_log_time > LOG_THRESHOLD_TIME:
                         logger.warning(
                             "Waited on the process pool longer than %d seconds. Waiting for %d results. Pool: %r",
                             LOG_THRESHOLD_TIME,
                             len(self.__processes),
                             self.__pool,
                         )
-                        self.__pool_waiting_time = current_time
+                        self.__pool_waiting_log_time = current_time
+                    if current_time - self.__pool_waiting_time > FAIL_THRESHOLD_TIME:
+                        raise multiprocessing.TimeoutError(
+                            f"Waited on process pool for {FAIL_THRESHOLD_TIME} seconds giving up."
+                        ) from err
                 break
             else:
                 self.__pool_waiting_time = None
+                self.__pool_waiting_log_time = None
 
     def __check_for_results_impl(self, timeout: Optional[float] = None) -> None:
         (
