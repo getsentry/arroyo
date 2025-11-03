7b48e51d7ed6b2ad8a39e14390b667f5f9ff8f6c
Tony Le <tony.le@sentry.io>
2025-11-03 17:46:49 -0500

    Remove run_task_with_multiprocessing.processes metric

--- arroyo/utils/metric_defs.py
+++ arroyo/utils/metric_defs.py
@@ -44,9 +44,6 @@ MetricName = Literal[
     "arroyo.strategies.run_task_with_multiprocessing.pool_size",
     # Counter: A subprocess by multiprocessing unexpectedly died.
     "sigchld.detected",
-    # Gauge: Shows how many processes the multiprocessing strategy is
-    # configured with.
-    "arroyo.strategies.run_task_with_multiprocessing.processes",
     # Counter: Incremented when the multiprocessing pool is created (or re-created).
     "arroyo.strategies.run_task_with_multiprocessing.pool.create",
     # Time: (unitless) spent polling librdkafka for new messages.
