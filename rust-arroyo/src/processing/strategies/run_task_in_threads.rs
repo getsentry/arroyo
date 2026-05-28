1dc1752e4a2e040b449199dd003056a5f07233b1
MeredithAnya <meredith@sentry.io>
2026-05-28 10:09:45 -0700

    ref(metrics): remove too_many_handles metric (#539)

--- rust-arroyo/src/processing/strategies/run_task_in_threads.rs
+++ rust-arroyo/src/processing/strategies/run_task_in_threads.rs
@@ -208,7 +208,6 @@ where
         }
 
         if self.handles.len() > self.concurrency {
-            counter!("arroyo.strategies.run_task_in_threads.too_many_handles", 1, "strategy_name" => self.metric_strategy_name);
             return Err(SubmitError::MessageRejected(MessageRejected { message }));
         }
 
