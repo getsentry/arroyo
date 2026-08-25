0757e36a2199e4d5e996850a9701a69ed1d326dc
tryangul <11639460+tryangul@users.noreply.github.com>
2026-08-25 12:22:36 -0700

    feat(reduce): track batch trigger reason on batch_time metric.

--- rust-arroyo/src/processing/strategies/reduce.rs
+++ rust-arroyo/src/processing/strategies/reduce.rs
@@ -191,14 +191,19 @@ impl<T, TResult> Reduce<T, TResult> {
         }
 
         let batch_time = self.batch_state.batch_start_time.elapsed();
-        let batch_complete = self.batch_state.message_count >= self.max_batch_size
-            || batch_time >= self.max_batch_time;
+        let size_trigger_complete = self.batch_state.message_count >= self.max_batch_size;
+        let time_trigger_complete = batch_time >= self.max_batch_time;
+        let batch_complete = size_trigger_complete || time_trigger_complete;
 
         if !batch_complete && !force {
             return Ok(());
         }
 
-        timer!("arroyo.strategies.reduce.batch_time.ms", batch_time);
+        timer!(
+            "arroyo.strategies.reduce.batch_time.ms",
+            batch_time,
+            "trigger_reason" => if size_trigger_complete { "size" } else { "time" }
+        );
 
         let batch_state = mem::replace(
             &mut self.batch_state,
